ea77090718cfb4811d2766b34665b5a08f68454d
nadia <nadia@com.ua>
2025-02-14 09:48:48 +0100

    update API test

--- src/test/java/SmokeApiTests.java
+++ src/test/java/SmokeApiTests.java
@@ -1,16 +1,29 @@
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class SmokeApiTests {
     private static final String BASE_URL = "https://petstore.swagger.io/v2/";
+    String body = """
+                {
+                  "id": 0,
+                  "username": "string",
+                  "firstName": "string",
+                  "lastName": "string",
+                  "email": "string",
+                  "password": "string",
+                  "phone": "string",
+                  "userStatus": 0
+                }""";
 
     @Test
     void createUserTest() {
         //given - config
         //when - http
         //then - assert
-        String bodyJson = "{\n" +
+        /*String body = "{\n" +
                 "  \"id\": 0,\n" +
                 "  \"username\": \"string\",\n" +
                 "  \"firstName\": \"string\",\n" +
@@ -19,15 +32,36 @@ public class SmokeApiTests {
                 "  \"password\": \"string\",\n" +
                 "  \"phone\": \"string\",\n" +
                 "  \"userStatus\": 0,\n" +
-                "}";
+                "}";*/
+
+
 
 
         ValidatableResponse response = given()
                 .header("accept", "application/json")
                 .header("Content-Type", "application/json")
-                .body(bodyJson)
+                .body(body)
                 .when().post(BASE_URL + "user").then();
-        response.statusCode(400);
+        response.statusCode(200);
         response.extract().response().prettyPrint();
     }
+
+    @Test
+    void checkUserResponseBody() {
+
+                 given()
+                .baseUri(BASE_URL)
+                .header("accept", "application/json")
+                .header("Content-Type", "application/json")
+                .body(body).
+                when()
+                .post("user")
+                .then()
+                .statusCode(200)
+                .body("code",equalTo(200))
+                .body("type", equalTo("unknown"))
+                .body("message", notNullValue(String.class));
+    }
+
+
 }
